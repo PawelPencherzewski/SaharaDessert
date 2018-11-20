7d691aff91d17781ac5c5d611d63c13f4ffd8fe1
PawelPencherzewski <14199041@studentmail.ul.ie>
2018-11-20 22:21:18 +0000

    Add files via upload

---
+++ user/LogIn.java
@@ -0,0 +1,51 @@
+package user;
+
+public class LogIn {
+	
+	public static void main(String[] args){
+		// Scan user file for existing usernames.
+		boolean found = ScanForExistingUser.scan(args[1]);
+
+		// Login/Register logic
+		if(found == true){                                                        // IF USER EXISTS IN DATABASE
+			if(args[0].equals("login")){
+				if(args[1].startsWith("c.")){                             // CUSTOMER LOGIN
+                                        UserFactory factory = new CustomerUserFactory();
+					User customer1 = factory.createUser("customer");
+					customer1.login(args[1]);
+				}
+				else if(args[1].startsWith("s.")){                       // STAFF LOGIN
+                                        UserFactory factory = new StaffUserFactory();
+					User staff1 = factory.createUser("staff");
+					staff1.login(args[1]);
+                                }
+				else
+					System.out.println("Unrecognised user type.");
+			}
+			else if(args[0].equals("register"))
+                            System.out.println("Username already taken.");
+			else
+                            System.out.println("Command not recognized. Try login or register.");
+		}
+		else{                                                                     // IF USER DOESNT EXIST IN DATABASE
+			if(args[0].equals("register")){
+				if(args[1].startsWith("c.")){                             // CUSTOMER REGISTER
+                                        UserFactory factory = new CustomerUserFactory();
+					User customer1 = factory.createUser("customer");
+					customer1.register(args[1]);
+				}
+				else if(args[1].startsWith("s.")){                        // STAFF REGISTER
+                                        UserFactory factory = new StaffUserFactory();
+					User staff1 = factory.createUser("staff");
+					staff1.register(args[1]);
+                                }
+				else
+					System.out.println("Unrecognised user type.");
+			}
+			else if(args[0].equals("login"))
+                            System.out.println("Username doesn't exist.");
+			else
+                            System.out.println("Command not recognized. Try login or register.");
+		}
+	}
+}
